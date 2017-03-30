2d1a8c8a3397c1d0bc50041fb18f74a78ac2599f
Logan Komanetz <Logan.Komanetz@networkedenergy.com>
2017-03-30 09:51:13 -0500

    Adding the loggable class stuff into the AutoLog class.

--- src/AutoLog/AutoLog.cs
+++ src/AutoLog/AutoLog.cs
@@ -7,7 +7,20 @@ using System.Reflection;
 namespace AutoLogger {
 
 	public static class AutoLog {
-		
+
+		private static LoggableClassFinder _classFinder;
+		private static IList<LoggableClass> _loggableClasses;
+
+		public static void With(Assembly assembly) {
+			_classFinder = new LoggableClassFinder(assembly);
+			_loggableClasses = _classFinder.FindLoggableClasses();
+		}
+
+		public static void With(IList<Assembly> assemblies) {
+			_classFinder = new LoggableClassFinder(assemblies);
+			_loggableClasses = _classFinder.FindLoggableClasses();
+		}
+
 	}
 
 }
